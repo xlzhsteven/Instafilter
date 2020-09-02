5be9d7ab3d18a037137767b52cedf046d68eb9e5
Xiaolong Zhang <xlzhsteven@gmail.com>
2020-09-01 21:20:39 -0700

    Project 13 part 4
    Building our basic UI
    Importing an image into SwiftUI using UIImagePickerController
    Basic image filtering using Core Image

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,11 +6,95 @@
 //  Copyright © 2020 Xiaolong. All rights reserved.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    @State var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
-        Text("Hello, World!")
+        // Create Binding that returns filterIntensity when fetching
+        // When setting the value, update filterIntensity and also trigger applyProcessing() method
+        // This is needed since @State is struct, wrappedValue is nonmutating, we have to create a custom binding to provide our own code to run when the value is read or written
+        // https://www.hackingwithswift.com/books/ios-swiftui/creating-custom-bindings-in-swiftui for more details
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        
+        return NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    
+                    if image != nil {
+                        image?.resizable().scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true // Tap to show image picker
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        // save the picture
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
+            .sheet(isPresented: $showingImagePicker,
+                   onDismiss: loadImage) { // When sheet is dismissed, call loadImage()
+                ImagePicker(image: self.$inputImage) // Present ImagePicker in a sheet
+            }
+        }
+    }
+    
+    private func loadImage() {
+        guard let inputImage = inputImage else { return }
+        let beginImage = CIImage(image: inputImage) // Create CIImage from UIImage
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey) // Set input image to the filter
+        applyProcessing() // Process image
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity) // Set filter intensity with filterIntensity value
+        
+        guard let outputImage = currentFilter.outputImage else { return } // Get output input with CIImage type
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) { // Create CGImage fro CIImage
+            let uiImage = UIImage(cgImage: cgimg) // Create UIImage from CIImage
+            image = Image(uiImage: uiImage) // Save the transformed image to the image placeholder
+        }
     }
 }
 
---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,42 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Xiaolong Zhang on 9/1/20.
+//  Copyright © 2020 Xiaolong. All rights reserved.
+//
+
+import SwiftUI
+
+// Image picker that wraps a UIImagePickerController into a view that can be presented inside a SwiftUI view
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+}
